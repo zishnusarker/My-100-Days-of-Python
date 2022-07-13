2cbdf13baaeb71d16993bb6c3d189c8c622d9fee
zishnusarker <94640101+zishnusarker@users.noreply.github.com>
2022-07-13 17:08:20 +0530

    Putting all work in

---
+++ Day2/day-2-end/main.py
@@ -0,0 +1,14 @@
+from numpy import double
+
+
+s= input(print("Give an Integer"))
+print(type(int(s)))
+
+p= input(print("\nGive an float"))
+print(type(float(p)))
+
+q= input(print("\nGive an double"))
+print(type(double(q)))
+
+r= input(print("\nGive an string"))
+print(type(string(r)))

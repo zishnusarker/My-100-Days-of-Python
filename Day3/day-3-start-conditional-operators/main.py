b88864f64d28c14e90384b0117416d5b91ef70ab
zishnusarker <94640101+zishnusarker@users.noreply.github.com>
2022-07-21 00:22:47 +0530

    changes

--- Day3/day-3-start-conditional-operators/main.py
+++ Day3/day-3-start-conditional-operators/main.py
@@ -1,7 +1,10 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-if height >= 120 :   # operator <, <=, >=, ==, >, != 
+# if and else are conditional statement here 
+
+if height >= 120:   # operator <, <=, >=, ==, >, != 
   print ("You can ride a rollercoaster !!!")
 else:
   print("Sorry, you are not allowed to ride a rollercoaster. ")
+  

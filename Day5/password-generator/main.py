04ee10b096964b374d4772609cbfcaf95dbf2278
zishnusarker <94640101+zishnusarker@users.noreply.github.com>
2022-09-09 00:28:31 +0530

    commit in proect

--- Day5/password-generator/main.py
+++ Day5/password-generator/main.py
@@ -9,20 +9,51 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = []
+
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
 for char in range(0, nr_letters):
+  # password += random.choice(letters)
   p = random.choice(letters)
-  password = password + list(p)
+  password = password + p
+  
 for char in range (0, nr_symbols):
+  # password += random.choice(symbols)
   q = random.choice(symbols)
-  password = password + list(q)
+  password = password + q
+  
 for char in range(0,nr_numbers):
+  #password += random.choice(numbers)
   r = random.choice(numbers)
-  password = password + list(r)
+  password = password + r
+  
 print(password)
-random.shuffle(password)
-print (password)
+
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+password_list = []
+for char in range(0, nr_letters):
+   password_list += random.choice(letters)
+  # password_list.appened(random.choice(letters))
+  
+for char in range (0, nr_symbols):
+  password_list += random.choice(symbols)
+  # password_list.appened(random.choice(letters))
+  
+for char in range(0,nr_numbers):
+  password_list += random.choice(numbers)
+  #password_list.appened(random.choice(numbers))
+  
+print(password_list)
+
+
+random.shuffle(password_list)
+print (password_list)
+
+password1 = ""
+for char in password_list:
+  password1 += char 
+
+print(f"your final password is :{password1}")

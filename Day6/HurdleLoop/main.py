f3a6ad69a8b681c38a4edc0657107ea81c2c3070
zishnusarker <94640101+zishnusarker@users.noreply.github.com>
2022-09-15 10:16:01 +0530

    all modified

---
+++ Day6/HurdleLoop/main.py
@@ -0,0 +1,16 @@
+def jump():
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+  
+for go in range(0,6):
+    move()
+    jump()
